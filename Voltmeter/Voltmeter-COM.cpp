1bcf9e4ba197eb01d29a07e2266c7570d43d21c4
dymon <obuvalin@gmail.com>
2011-08-08 18:08:55 +0400

    add COM implementations

---
+++ Voltmeter/Voltmeter-COM.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "math.h"
+#include "PCL_Static.h"
+#include "Voltmeter.h"
+#include "Calibrator.h"
+
+static Voltmeter *pVRef = new Voltmeter(Voltmeter::REFERENCE);
+static Voltmeter *pVVer = new Voltmeter(Voltmeter::VERIFIED);
+static Calibrator *pC = getCalibrator();
+
+typedef struct {
+	double A, B, C;
+} data_t;
+
+#define _(X) (((data_t*)m_data)->X)
+	
+Voltmeter::Voltmeter(Purpose Which)
+{
+	printf("Creating COM%d voltmeter\n", Which);
+	m_data = new(data_t);
+	double LRandomFromTo(double From, double To);
+	_(A) = LRandomFromTo(0.001, 0.01);
+	_(B) = LRandomFromTo(0.01, 1);
+	_(C) = LRandomFromTo(0.01, 0.9);
+}
+
+Voltmeter::~Voltmeter()
+{
+	delete m_data;
+}
+
+VOLTMETER_API Voltmeter *getVoltmeter(Voltmeter::Purpose Which)
+{
+	if (Which == Voltmeter::REFERENCE) return pVRef;
+	if (Which == Voltmeter::VERIFIED) return pVVer;
+	LException abort(ZShow);
+	abort.Signal(LFormat("Wrong voltmeter purpose code %d\n", Which));
+	LTerminate("Metrology abort during votmeter initialization");
+	return 0;
+}
+
+double Voltmeter::getVoltage()
+{
+	return _(A) * pC->getVoltage() + _(B) * abs(sin((double)pC->getFrequency())*0.01) + _(C);
+}
