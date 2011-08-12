5ac2738fd4412d0f0a8009bc0ef077a0ddfdd93b
dymon <obuvalin@gmail.com>
2011-08-12 12:52:41 +0400

    smooth raise in runUpExact

--- Inspector/SafeCalibrator.cpp
+++ Inspector/SafeCalibrator.cpp
@@ -12,6 +12,7 @@ double runUpExact(double Voltage, uint Frequency, Calibrator *pC)
 	const double k = 0.3333333333333333;
 	const double k2 = 0.1;
 
+
 	if (pC->getFrequency() != Frequency) { //сбрасываем на 0 если изменилась частота
 		pC->setOutput(0);
 		Uout = pC->setVoltage(0);
@@ -23,14 +24,11 @@ double runUpExact(double Voltage, uint Frequency, Calibrator *pC)
 	}
 	if (!pC->getOutput()) pC->setOutput(1);
 
-	while (fabs(Voltage - Uout) > barrier) { // крутим пока не достигнут минимальный порог
-		if (fabs(Voltage - Uout) < delta) {
-			Uout += ((Voltage - Uout) * k);
-		}
-		else {
-			Uout += ((Voltage - Uout) * k2);
-		}
-		pC->setVoltage(Uout); // устанавливает напряжение на выходе калибратора
+	double kk = 0.3;
+	while (fabs(Voltage - Uout) > barrier) {
+		Uout += ((Voltage - Uout) * kk);
+		kk += 0.1;
+		pC->setVoltage(Uout);
 	}
 
 	return pC->setVoltage(Voltage);
